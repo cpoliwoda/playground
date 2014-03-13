030c7e849c06260c71fe6c98033d3a4e90531460
Christian Poliwoda <christianpoliwoda@yahoo.de>
2014-03-13 17:06:03 +0100

    tmp commit to save work, layoutComparator, allShapes are sorted now

---
+++ src/main/java/p03raster/FXwindow04cursorJumpOverListDynamic.java
@@ -0,0 +1,247 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p03raster;
+
+import java.util.ArrayList;
+import java.util.Random;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import p03raster.util.LayoutXYZcomparator;
+
+/**
+ *
+ * @author Christian Poliwoda <christian.poliwoda@gcsc.uni-frankfurt.de>
+ */
+public class FXwindow04cursorJumpOverListDynamic extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        int sceneWidth = 600;
+        int sceneHeight = 400;
+        Group root = new Group();
+        Scene scene = new Scene(root, sceneWidth, sceneHeight, Color.BLACK);
+        stage.setScene(scene);
+
+        int idCounter = 1;
+
+        Circle circle01 = new Circle(30, Color.BEIGE);
+        //set position of the shape
+        circle01.setLayoutX(100);
+        circle01.setLayoutY(180);
+
+        circle01.setId(Integer.toString(idCounter));
+        idCounter++;
+
+        Random random = new Random();
+        
+        ArrayList<Shape> circlesInSecColumn = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Circle circle02 = new Circle(30 + i * 3, Color.BLUEVIOLET);
+            //set position of the shape
+            circle02.setLayoutX(random.nextDouble()*sceneWidth);
+            circle02.setLayoutY(50 + 100 * i);
+            circlesInSecColumn.add(circle02);
+
+            circle02.setId(Integer.toString(idCounter));
+            idCounter++;
+        }
+
+        ArrayList<Shape> circlesInThirdColumn = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Circle circle03 = new Circle(20 + i * 2, Color.BLUE);
+            //set position of the shape
+            circle03.setLayoutX(random.nextDouble()*sceneWidth);
+            circle03.setLayoutY(50 + 100 * i);
+            circlesInSecColumn.add(circle03);
+
+            circle03.setId(Integer.toString(idCounter));
+            idCounter++;
+        }
+
+        ArrayList<Shape> allCircles = new ArrayList<>();
+        allCircles.add(circle01);
+        allCircles.addAll(circlesInSecColumn);
+        allCircles.addAll(circlesInThirdColumn);
+        //sort the circles 
+        allCircles.sort(new LayoutXYZcomparator());
+
+        //add elements to root element of scene
+        root.getChildren().addAll(allCircles);
+
+        //show window
+        stage.show();
+
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, new KeyMoveOverListEventHandler04(root, allCircles));
+
+    }
+
+}
+
+class KeyMoveOverListEventHandler04 implements EventHandler<KeyEvent> {
+
+    ArrayList<Shape> allShapes = new ArrayList<>();
+    Circle cursor = new Circle(10, Color.BROWN);
+    ArrayList<ArrayList<Shape>> allColumns = new ArrayList<>();
+    int range = 50;
+
+    public KeyMoveOverListEventHandler04(Group root, ArrayList<Shape> allCircles) {
+        allShapes.addAll(allCircles);
+//        allShapes.sort(new LayoutXYZcomparator());
+        
+        Shape firstShape = allShapes.get(0);
+        root.getChildren().add(cursor);
+
+        /* Attention:
+         Depending on how the shape was positionized (center.. or translate..) we need to
+         use a different "methode" to positionize our cursor !!! 
+         */
+        Point2D coordinatesOfFirst = new Point2D(
+                firstShape.getLayoutX(), firstShape.getLayoutY());
+
+//        System.out.println("firstShape.getId() = " + firstShape.getId() + " , " + firstShape.getFill());
+//        System.out.println("coordinatesOfFirst = " + coordinatesOfFirst.getX() + " , " + coordinatesOfFirst.getY());
+        cursor.setLayoutX(coordinatesOfFirst.getX());
+        cursor.setLayoutY(coordinatesOfFirst.getY());
+        
+//        sortShapesIntoColumns();
+    }
+
+    public void sortShapesIntoColumns() {
+        //
+        // sort the the shapes into columns
+        //
+        //start with new first column and pick the first shape as basis for comparison
+        allColumns.add(new ArrayList<>());
+        int columnIndex = 0;
+        Shape firstShapeInAcolumn = this.allShapes.get(0);
+        allColumns.get(0).add(firstShapeInAcolumn);//add the first shape in the first column manually
+        
+        
+        //check now all other shapes if they are in range 
+        //if yes add to latest column
+        //if not create next new column and add to it and select new shape for comparison 
+        for (int i = 1; i < this.allShapes.size(); i++) {
+            Shape nextShape = this.allShapes.get(i);
+
+            //check if next shape is in x direction near enough
+            if (nextShape.getLayoutX() - range <= firstShapeInAcolumn.getLayoutX()) {
+                allColumns.get(columnIndex).add(nextShape);
+            } else {
+                columnIndex++;
+                allColumns.add(new ArrayList<>());
+                allColumns.get(columnIndex).add(nextShape);
+                firstShapeInAcolumn = nextShape;
+            }
+        }
+        System.out.println(" columnIndex = " + columnIndex);
+    }
+
+    @Override
+    public void handle(KeyEvent event) {
+
+         sortShapesIntoColumns();
+         
+        // if arrow key
+        if ((event.getCode().equals(KeyCode.UP))
+                || (event.getCode().equals(KeyCode.DOWN))
+                || (event.getCode().equals(KeyCode.RIGHT))
+                || (event.getCode().equals(KeyCode.LEFT))) {
+
+//            System.out.println(" a arrow key was pressed ");
+            Shape shapeMarkedByCursor = null;
+
+            //find the shape under the cursor
+            for (int i = 0; i < allShapes.size(); i++) {
+                shapeMarkedByCursor = allShapes.get(i);
+
+                boolean containsCursorCenter = shapeMarkedByCursor.contains(
+                        shapeMarkedByCursor.sceneToLocal(cursor.getLayoutX(), cursor.getLayoutY()));
+
+                //save performance if shape is found we do not need to search the rest of the list
+                if (containsCursorCenter) {
+                    break;
+                }
+            }
+
+            if ((event.getCode().equals(KeyCode.UP))
+                    || (event.getCode().equals(KeyCode.DOWN))) {
+
+                int indexOfShape = allShapes.indexOf(shapeMarkedByCursor);
+                Shape nextShape = null;
+
+                if (event.getCode().equals(KeyCode.UP)) {
+                    System.out.println(" UP key was pressed ");
+                    if (indexOfShape > 0) {
+                        nextShape = allShapes.get(indexOfShape - 1);
+                    }
+                } else {//DOWN
+                    System.out.println(" DOWN key was pressed ");
+                    if (indexOfShape < allShapes.size() - 1) {
+                        nextShape = allShapes.get(indexOfShape + 1);
+                    }
+                }
+
+                if (nextShape != null) {
+                    cursor.setLayoutX(nextShape.getLayoutX());
+                    cursor.setLayoutY(nextShape.getLayoutY());
+                }
+
+            } else { // LEFT or RIGHT
+
+//                System.out.println(" LEFT or RIGHT key was pressed ");
+//                System.out.println(" shapeMarkedByCursor = " + shapeMarkedByCursor);
+
+                int columnIndexOfCursor = -1;//invalid value as default
+
+                //check in which column the cursor is
+                for (int i = 0; i < allColumns.size(); i++) {
+                    boolean columnContainsCursor = allColumns.get(i).contains(shapeMarkedByCursor);
+
+                    if (columnContainsCursor) {
+                        columnIndexOfCursor = i;
+                        System.out.println("columnIndexOfCursor = " + columnIndexOfCursor);
+                        break;
+                    }
+                }
+
+                Shape firstShapeInNextColumn = null;
+
+                if (event.getCode().equals(KeyCode.LEFT)) {
+                    System.out.println(" LEFT  key was pressed ");
+                    
+                    if (columnIndexOfCursor > 0) {
+                        firstShapeInNextColumn = allColumns.get(columnIndexOfCursor - 1).get(0);
+                    }
+
+                } else // RIGHT
+                {
+                    System.out.println(" RIGHT key was pressed ");
+                    if ((columnIndexOfCursor > - 1)//we really found a shape
+                            && (columnIndexOfCursor < allColumns.size() - 1)) {
+
+                        firstShapeInNextColumn = allColumns.get(columnIndexOfCursor + 1).get(0);
+                    }
+                }
+                if (firstShapeInNextColumn != null) {
+                    cursor.setLayoutX(firstShapeInNextColumn.getLayoutX());
+                    cursor.setLayoutY(firstShapeInNextColumn.getLayoutY());
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ src/main/java/p03raster/util/LayoutXYZcomparator.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p03raster.util;
+
+import java.util.Comparator;
+import javafx.scene.shape.Shape;
+
+/**
+ *
+ * @author Christian Poliwoda <christian.poliwoda@gcsc.uni-frankfurt.de>
+ */
+public class LayoutXYZcomparator implements Comparator<Shape> {
+
+    @Override
+    public int compare(Shape o1, Shape o2) {
+        /*
+         negativer Rückgabewert: Der erste Parameter ist untergeordnet
+         0 als Rückgabewert: Beide Parameter werden gleich eingeordnet
+         positiver Rückgabewert: Der erste Parameter ist übergeordnet
+         */
+        if (o1 == null || o2 == null) {
+
+            if (o1 == null) {
+                return -1;
+            } else if (o2 == null) {
+                return 1;
+            }
+            return 0;
+
+        } else {
+
+            // check first X than Y
+            if (o1.getLayoutX() > o2.getLayoutX()) {
+                return 1;
+            } else if (o1.getLayoutX() == o2.getLayoutX()) {
+
+                if (o1.getLayoutY() > o2.getLayoutY()) {
+                    return 1;
+                } else if (o1.getLayoutY() == o2.getLayoutY()) {
+                    return 0;
+                } else if (o1.getLayoutY() < o2.getLayoutY()) {
+                    return -1;
+                }
+            } else if (o1.getLayoutX() < o2.getLayoutX()) {
+                return -1;
+            }
+        }// null
+        return 0;
+    }
+}
